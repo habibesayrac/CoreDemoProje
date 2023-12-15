a9868272147864796df406c2f0d058ff2ee1e17e
habibesayrac <habibesayrac@outlook.com>
2023-12-15 14:18:16 +0300

    efrepository created

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class,new()
+    {
+        void TAdd(Category category);
+        void TDelete(Category category);
+        void CategoryUpdate(Category category);
+        List<Category> GetList();
+        Category GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository _efCategoryRepository;
+
+        public CategoryManager(EfCategoryRepository efCategoryRepository)
+        {
+            _efCategoryRepository = efCategoryRepository;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+          _efCategoryRepository.Insert(category);
+
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _efCategoryRepository.Delete(category);
+
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _efCategoryRepository.Update(category);
+
+        }
+
+        public Category GetById(int id)
+        {
+           return _efCategoryRepository.GetByID(id);
+
+        }
+
+        public List<Category> GetList()
+        {
+            return _efCategoryRepository.GetListAll();
+        }
+    }
+}
