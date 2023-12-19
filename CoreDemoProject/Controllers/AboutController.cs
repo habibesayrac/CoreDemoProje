c16b158cf914ad713dcd858c224bb43c7edb8d45
habibesayrac <habibesayrac@outlook.com>
2023-12-19 21:02:16 +0300

    About page added

---
+++ CoreDemoProject/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemoProject.Controllers
+{
+	public class AboutController : Controller
+	{
+		AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult SocialMediaAbout()
+		{
+			var values = aboutManager.GetList();
+			return PartialView(values);
+		}
+	}
+}
