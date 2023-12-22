98dbe2e136246f10e46b9d90af15d1ecb30a8303
habibesayrac <habibesayrac@outlook.com>
2023-12-22 14:28:46 +0300

    sidebar updated

--- CoreDemoProject/Controllers/WriterController.cs
+++ CoreDemoProject/Controllers/WriterController.cs
@@ -22,5 +22,18 @@ namespace CoreDemoProject.Controllers
         {
             return View();
         }
+
+        [AllowAnonymous]
+
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
     }
 }
