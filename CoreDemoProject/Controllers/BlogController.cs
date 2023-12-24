0d376083684f8eaee066d46dc552601173b2e8ce
habibesayrac <habibesayrac@outlook.com>
2023-12-24 13:35:22 +0300

    BlogAdd page added

--- CoreDemoProject/Controllers/BlogController.cs
+++ CoreDemoProject/Controllers/BlogController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CoreDemoProject.Controllers
 {
@@ -23,8 +27,42 @@ namespace CoreDemoProject.Controllers
         }
         public IActionResult BlogListByWriter()
         {
-           var values=  blogManager.GetBlogByWriter(1);
+            var values = blogManager.GetBlogByWriter(1);
             return View(values);
         }
+       
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            //List<SelectListItem> 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator bv = new BlogValidator();
+
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterID = 1;
+
+                blogManager.TAdd(p);
+
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+       
     }
 }
