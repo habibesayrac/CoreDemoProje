4da39e8cc94a54b642dae2967bb23958363165b5
habibesayrac <habibesayrac@outlook.com>
2023-12-26 10:03:16 +0300

    Dashboardpage category area added

---
+++ CoreDemoProject/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemoProject.ViewComponents.Category
+{
+    public class CategoryListDashboard :ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
