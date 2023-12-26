04c45491477d01f6733fe558a9808cfa77b2aca5
habibesayrac <habibesayrac@outlook.com>
2023-12-26 13:09:34 +0300

    Blograting table added

---
+++ DataAccessLayer/Migrations/20231226100003_mig_add_comment_score_column.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_comment_score_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Newsletters",
+                table: "Newsletters");
+
+            migrationBuilder.RenameTable(
+                name: "Newsletters",
+                newName: "NewsLetters");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BlogScore",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NewsLetters",
+                table: "NewsLetters",
+                column: "MailID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NewsLetters",
+                table: "NewsLetters");
+
+            migrationBuilder.DropColumn(
+                name: "BlogScore",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "NewsLetters",
+                newName: "Newsletters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Newsletters",
+                table: "Newsletters",
+                column: "MailID");
+        }
+    }
+}
