6427875819d2fc86f829782d3c62b4b289f28f5f
habibesayrac <habibesayrac@outlook.com>
2023-12-28 13:54:00 +0300

    Notification page added

--- CoreDemoProject/Controllers/NotificationController.cs
+++ CoreDemoProject/Controllers/NotificationController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemoProject.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var values = notificationManager.GetList();
+            return View(values);
         }
     }
 }
