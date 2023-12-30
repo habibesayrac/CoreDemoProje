a069bd6a0ace2446f9cc0655dd902fdd6a5b6b52
habibesayrac <habibesayrac@outlook.com>
2023-12-30 14:39:34 +0300

    ınbox page added

---
+++ CoreDemoProject/Controllers/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CoreDemoProject.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Repository());
+        public IActionResult InBox()
+        {
+            int id = 2;
+            var values = message2Manager.GetInboxListByWriter(id);
+            return View(values);
+        }
+
+        public IActionResult MessageDetails(int id)
+        {
+            var value = message2Manager.TGetById(id);
+
+            return View(value);
+        }
+    }
+
+}
