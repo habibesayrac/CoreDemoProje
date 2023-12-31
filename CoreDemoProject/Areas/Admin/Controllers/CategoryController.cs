0cf596cd985ce3282e67a066d31c197cd52718c2
habibesayrac <habibesayrac@outlook.com>
2023-12-31 19:29:27 +0300

    categoryadd added

--- CoreDemoProject/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemoProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,12 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+
+using X.PagedList;
 
 namespace CoreDemoProject.Areas.Admin.Controllers
 {
@@ -8,10 +14,39 @@ namespace CoreDemoProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var values = categoryManager.GetList();
+            var values = categoryManager.GetList().ToPagedList(page, 3);
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+
+            CategoryValidator cv = new CategoryValidator();
+
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                categoryManager.TAdd(p);
+
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
     }
 }
