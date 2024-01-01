cf5b325f1d52acee93adefff54f0af3acddb76d0
habibesayrac <habibesayrac@outlook.com>
2024-01-01 14:05:03 +0300

    exceldynamic report added

--- CoreDemoProject/Areas/Admin/Controllers/BlogController.cs
+++ CoreDemoProject/Areas/Admin/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using CoreDemoProject.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemoProject.Areas.Admin.Controllers
@@ -46,5 +47,47 @@ namespace CoreDemoProject.Areas.Admin.Controllers
 		{
 			return View();
 		}
+	
+		public IActionResult ExportDynamicExcelBlogList()
+		{
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("BlogListesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+        }
+		public List<BlogModel2> BlogTitleList()
+		{
+			List<BlogModel2> bm = new List<BlogModel2>();
+			using(var c = new Context())
+			{
+				bm = c.Blogs.Select(x => new BlogModel2
+				{
+					ID= x.BlogID,
+					BlogName = x.BlogTitle
+				}).ToList();
+			}
+			return bm;
+		}
+		public IActionResult BlogTitleListExcel()
+		{
+			return View();
+		}
+	
 	}
 }
