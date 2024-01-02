2ea8b2be418f328160fc7319e4ffc9af8d7cb78f
habibesayrac <habibesayrac@outlook.com>
2024-01-02 09:43:40 +0300

    admin page edited

---
+++ CoreDemoProject/Areas/ViewComponents/Statistic/Statistic3.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemoProject.Areas.ViewComponents.Statistic
+{
+    public class Statistic3:ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+           
+            return View();
+        }
+    }
+}
