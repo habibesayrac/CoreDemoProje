277a456a97aaab27a7e615896bb885cf179844eb
habibesayrac <habibesayrac@outlook.com>
2024-01-02 10:41:29 +0300

    chart area created

---
+++ CoreDemoProject/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using CoreDemoProject.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemoProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass {
+                CategoryName="Teknoloji",
+                CategoryCount=10
+            });
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Spor",
+                CategoryCount = 5
+            });
+            return Json(new {jsonlist = list });
+        }
+    }
+}
---
+++ CoreDemoProject/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreDemoProject.Areas.Admin.Models
+{
+    public class CategoryClass
+    {
+        public int CategoryCount { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
