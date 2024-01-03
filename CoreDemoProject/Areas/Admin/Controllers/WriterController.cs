ddbd8f7e2020a8f63bdb4ae213eac1211b42a572
habibesayrac <habibesayrac@outlook.com>
2024-01-03 21:06:31 +0300

    ajax completed

--- CoreDemoProject/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemoProject/Areas/Admin/Controllers/WriterController.cs
@@ -33,6 +33,22 @@ namespace CoreDemoProject.Areas.Admin.Controllers
 
         }
 
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x=>x.Id==id);
+            writers.Remove(writer);
+            return Json(writer);
+
+        }
+
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(jsonWriter);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
