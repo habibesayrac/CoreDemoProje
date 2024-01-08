5990dde5972bcd336104983273de8fa8f2153ca4
habibesayrac <habibesayrac@outlook.com>
2024-01-08 09:38:48 +0300

    register added

--- CoreDemoProject/Program.cs
+++ CoreDemoProject/Program.cs
@@ -1,9 +1,14 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
