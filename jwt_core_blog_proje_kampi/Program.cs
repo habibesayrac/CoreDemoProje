8ddb65d773abc1e670da248d12e2e2222c56cf32
habibesayrac <habibesayrac@outlook.com>
2024-01-16 13:19:12 +0300

    jwt added and build token added

--- jwt_core_blog_proje_kampi/Program.cs
+++ jwt_core_blog_proje_kampi/Program.cs
@@ -10,16 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x=>
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
 {
-    x.RequireHttpsMetadata= false;
+    x.RequireHttpsMetadata = false;
     x.TokenValidationParameters = new TokenValidationParameters
     {
-        ValidIssuer="http://localhost",
-        ValidAudience="http://localhost",
+        ValidIssuer = "http://localhost",
+        ValidAudience = "http://localhost",
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("aspnetcoreprojekampi")),
         ValidateIssuerSigningKey = true,
-        ValidateLifetime= true,
+        ValidateLifetime = true,
         ClockSkew = TimeSpan.Zero
     };
 });
@@ -32,6 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseAuthentication();
 
 app.UseAuthorization();
 
